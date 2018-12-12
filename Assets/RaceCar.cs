1dfd5565dcc098b329ab16a31bd51ef82f473979
Unknown <mateo.bares@gmail.com>
2018-12-13 00:44:24 +0100

    Race screen and result screen implementation

---
+++ Assets/RaceCar.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceCar : MonoBehaviour
+{
+    public RaceStarter raceStarter;
+    [HideInInspector]
+    public float maxSpeed;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Rigidbody2D carRigidbody;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    private Vector2 startingPosition;
+    private float acceleration;
+    private float reactionTime;
+    private bool raceActive = false;
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (raceActive) {
+            carRigidbody.AddForce(Vector2.right * acceleration, ForceMode2D.Impulse);
+            if (carRigidbody.velocity.magnitude > maxSpeed) {
+                carRigidbody.velocity = Vector2.right * maxSpeed;
+            }
+        }
+    }
+
+    public void SetUpAndStartRace()
+    {
+        SetUp();
+        StartCoroutine(StartRace());
+    }
+
+    private void SetUp()
+    {
+        spriteRenderer.sprite = raceStarter.vehicleBody.sprite;
+        spriteRenderer.color = raceStarter.vehicleColor.color;
+        acceleration = raceStarter.tires.accelerationCoefficient *  10f * speed;
+        maxSpeed = speed * 2 + raceStarter.engine.maxSpeedCoefficient;
+        reactionTime = 1f / (2 + raceStarter.vehicleColor.reactionTimeCoefficient);
+        carRigidbody.drag = 1f / raceStarter.vehicleBody.aerodynamicsCoefficient;
+    }
+
+    private IEnumerator StartRace()
+    {
+        yield return new WaitForSeconds(reactionTime);
+        raceActive = true;
+    }
+
+    public void EndRace()
+    {
+        raceActive = false;
+        carRigidbody.velocity = Vector2.zero;
+    }
+
+    public void Reset()
+    {
+        EndRace();  
+        transform.position = startingPosition;
+    }
+}
---
+++ Assets/ResultScreenNavigation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScreenNavigation : MonoBehaviour
+{
+    [SerializeField]
+    private RaceStarter winner;
+    [SerializeField]
+    private Image winnerImage;
+    [SerializeField]
+    private GameObject resultScreen;
+    [SerializeField]
+    private GameObject startScreen;
+
+    public void ShowResultScreen()
+    {
+        winnerImage.sprite = winner.vehicleBody.sprite;
+        winnerImage.color = winner.vehicleColor.color;
+        resultScreen.SetActive(true);
+    }
+
+    public void HideResultScreen()
+    {
+        resultScreen.SetActive(false);
+    }
+
+    public void ShowStartScreen()
+    {
+        startScreen.SetActive(true);
+    }
+}
