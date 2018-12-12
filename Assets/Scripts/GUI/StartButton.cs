a9f75b182dd55ebac4c59a438c6a1edbf0acd4ca
Unknown <mateo.bares@gmail.com>
2018-12-12 17:48:51 +0100

    Start screen
    SImple start screen with title, start and quit buttons

---
+++ Assets/Scripts/GUI/StartButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startScreenPanel;
+
+    public void HideStartScreenPanel()
+    {
+        startScreenPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GameEvents/GameEventListener.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public EventResponsePair[] eventResponsePairs;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < eventResponsePairs.Length; ++i) {
+            eventResponsePairs[i].gameEvent.RegisterListener(this);
+        }
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < eventResponsePairs.Length; ++i) {
+            eventResponsePairs[i].gameEvent.UnregisterListener(this);
+        }
+    }
+
+    public void OnEventRaised(GameEvent gameEvent)
+    {
+        for (int i = 0; i < eventResponsePairs.Length; ++i) {
+            if (gameEvent == eventResponsePairs[i].gameEvent) {
+                eventResponsePairs[i].response.Invoke();
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class EventResponsePair
+{
+    public GameEvent gameEvent;
+    public UnityEvent response;
+}
